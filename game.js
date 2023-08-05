5153b3883526b2cc6f6100fcf9512c6b159ab0e3
tylerfire55 <tylergolpe@yahoo.com>
2023-08-04 20:16:10 -0400

    Moved internal JS to external

---
+++ game.js
@@ -0,0 +1,60 @@
+let playerScore = 0;
+let cpuScore = 0;
+
+function getComputerChoice() {
+    let cpuNum = Math.floor(Math.random() * 3);
+    if (cpuNum < 1) {
+        return "rock";
+    }
+    else if (cpuNum < 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+function rockPaperScissors(playerChoice, cpuChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    if (
+        (playerChoice == "rock" && cpuChoice == "paper") || 
+    (playerChoice == "paper" && cpuChoice == "scissors") || 
+    (playerChoice == "scissors" && cpuChoice == "rock")
+    ) {
+        cpuScore ++;
+        return `You lose! ${capitalizeFirst(cpuChoice)} beats ${playerChoice}!` 
+    }
+    else if (
+        (playerChoice == "rock" && cpuChoice == "rock") || 
+    (playerChoice == "paper" && cpuChoice == "paper") || 
+    (playerChoice == "scissors" && cpuChoice == "scissors")
+    ) {
+        return `It's a tie!`
+    }
+    else {
+        playerScore ++;
+        return `You win! ${capitalizeFirst(playerChoice)} beats ${cpuChoice}!` 
+    }
+}
+
+function capitalizeFirst(string){
+    return string.charAt(0).toUpperCase()+string.slice(1).toLowerCase()
+}
+
+function game(){
+    const cpuChoice = getComputerChoice();
+    const playerChoice = prompt();
+    console.log(rockPaperScissors(playerChoice, cpuChoice));
+    console.log(`Player ${playerScore} - ${cpuScore} CPU`);
+    if (playerScore == 3){
+        console.log("Game over! Player wins!")
+        playerScore = 0;
+        cpuScore = 0;
+    }
+    else if (cpuScore == 3){
+        console.log("Game over! CPU wins!")
+        playerScore = 0;
+        cpuScore = 0;
+    }
+    else {}
+}
