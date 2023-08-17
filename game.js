3b9579e5aefa13f56696507397e2e00c351cc2c0
tylerfire55 <tylergolpe@yahoo.com>
2023-08-17 19:04:29 -0400

    Updated the styles a bit. Need pictures for the choices...eventually?

--- game.js
+++ game.js
@@ -39,6 +39,7 @@ function rockPaperScissors(playerChoice, cpuChoice) {
     (playerChoice == "scissors" && cpuChoice == "rock")
     ) {
         cpuScore ++;
+        gameMessage.style.color = "orange";
         return `You lose! ${capitalizeFirst(cpuChoice)} beats ${playerChoice}!` 
     }
     else if (
@@ -46,10 +47,12 @@ function rockPaperScissors(playerChoice, cpuChoice) {
     (playerChoice == "paper" && cpuChoice == "paper") || 
     (playerChoice == "scissors" && cpuChoice == "scissors")
     ) {
+        gameMessage.style.color = "yellow";
         return `It's a tie!`
     }
     else {
         playerScore ++;
+        gameMessage.style.color = "skyblue";
         return `You win! ${capitalizeFirst(playerChoice)} beats ${cpuChoice}!` 
     }
 }
@@ -63,9 +66,11 @@ function game(){
     gameMessage.textContent = (rockPaperScissors(playerChoice, cpuChoice));
     if (playerScore == 3){
         gameMessage.textContent = ("Game over! Player wins!");
+        playAgain.style.backgroundColor = "skyblue";
     }
     else if (cpuScore == 3){
         gameMessage.textContent = ("Game over! CPU wins!")
+        playAgain.style.backgroundColor = "orange";
     }
     updateScore();
 }
@@ -73,9 +78,7 @@ function game(){
 function gameOver(){
     button.forEach(button => {
         button.disabled = true;
-        button.style.backgroundColor = "gray";
     })
-    playAgain.style.backgroundColor = "lime";
     playAgain.style.visibility = "visible";
     playAgain.disabled = false;
 }
@@ -83,7 +86,6 @@ function gameOver(){
 function enableButtons(){
     button.forEach(button => {
         button.disabled = false;
-        button.style.backgroundColor = "lime";
     })
 }
 
